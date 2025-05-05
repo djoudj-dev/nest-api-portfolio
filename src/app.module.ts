abf31458d389ea3223a33fa1914162e10b100a7f
djoudj-dev <nedellec.julien.78@gmail.com>
2025-05-05 13:58:19 +0200

    feat(config:build): Active CORS et ajoute le module de fichiers statiques
    - Active CORS dans l'application pour permettre les requêtes cross-origin.
    - Ajoute la configuration pour servir les fichiers statiques depuis le dossier `uploads`.
    - Déplace les constantes de l'authentification dans le module admin.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { BadgeContractModule } from './badge-contract/badge-contract.module';
+import { HeroModule } from './hero/hero.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AdminModule, PrismaModule, BadgeContractModule],
+  imports: [
+    AdminModule,
+    PrismaModule,
+    BadgeContractModule,
+    HeroModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
